0a8fe989ba49f1720d1ea45d0362985ecd23082a
Anthony Lamorte <lamorte@gmail.com>
2016-11-17 20:21:15 -0500

    Give tip calculator a 'UI'

--- ruby/1-tip-calculator/tip_calculator.rb
+++ ruby/1-tip-calculator/tip_calculator.rb
@@ -3,8 +3,8 @@ class TipCalculator
   attr_reader :total
 
   def initialize(bill_amount, tip_rate = 0)
-    @bill_amount = bill_amount
-    @tip_rate = tip_rate
+    @bill_amount = bill_amount.to_f
+    @tip_rate = tip_rate.to_i
   end
 
   def calculate
@@ -23,3 +23,13 @@ class TipCalculator
     @bill_amount + @tip
   end
 end
+
+puts 'What is the bill amount? '
+bill = gets.chomp
+puts 'What is the tip percentage? '
+tip = gets.chomp
+
+tip_calc = TipCalculator.new(bill, tip).calculate
+
+puts "The tip is $#{tip_calc.tip}"
+puts "The total is $#{tip_calc.total}"
